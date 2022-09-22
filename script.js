2577a62869874f26420a33b0562bbb8a7e786f99
Leonardo Irineu <leo_izidoro_@hotmail.com>
2022-09-22 08:53:36 -0300

    Contador JS

---
+++ script.js
@@ -0,0 +1,33 @@
+function contar(){
+    let inicio = document.getElementById('inicio')
+    let fim = document.getElementById('fim')
+    let passo = document.getElementById('passo')
+    let resultado = document.getElementById('res')
+    
+    if (inicio.value.length == 0 || fim.value.length == 0){
+        alert('[ERRO] dados incorretos')
+        resultado.innerHTML = "Preencha os dados acima!"
+    } 
+    else {
+        resultado.innerHTML = 'Contando <br>'
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if ( p <= 0 ){
+            alert('Considerando PASSO 1')
+            p = 1
+        }
+        if ( i < f){
+            //Contagem crescente
+            for(let c = i; c <= f; c += p) {
+                resultado.innerHTML += `${c} \u{1F449}` //emogi
+            }
+        } else {
+            //Contagem regressiva
+            for(let c = i; c >= f; c -= p){
+                resultado.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        resultado.innerHTML += `\u{1F3C1}`
+    }
+}
